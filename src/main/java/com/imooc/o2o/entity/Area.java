02d5a95c64351e5e3c83f07b5011cb8e2357f1c2
Xile Li <xli252@uottawa.ca>
2019-05-27 11:17:22 -0400

    shop web full stack program structure and setting

---
+++ src/main/java/com/imooc/o2o/entity/Area.java
@@ -0,0 +1,52 @@
+package src.main.java.com.imooc.o2o.entity;
+
+import javax.xml.crypto.Data;
+
+public class Area {
+
+    private Integer areaId;
+    private String areaName;
+    private Integer priority;
+    private Data createTime;
+    private Data lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Data getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Data createTime) {
+        this.createTime = createTime;
+    }
+
+    public Data getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Data lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+}
